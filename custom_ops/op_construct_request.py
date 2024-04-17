6730a95e312da2da7806955a9bb8a394652043f9
tianqyun111 <tqy965346240@gmail.com>
2024-04-17 13:56:13 +0000

    增加xl新链路

--- custom_ops/op_construct_request.py
+++ custom_ops/op_construct_request.py
@@ -236,6 +236,7 @@ class OpConstructRequest(object):
                 lora_prompts.extend(info_prompts)
                 
         lora_prompts = ", ".join(lora_prompts)
+        # lora_prompts = ""
         return lora_prompts
 
 
