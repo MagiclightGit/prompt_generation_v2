8bb89fb59ff1e4f76ff7adcf89ea8357a4e5987d
shonenada <shonenada@gmail.com>
2024-05-07 11:18:05 +0800

    feat: increse task concurrency

--- promptgpt_worker.py
+++ promptgpt_worker.py
@@ -218,7 +218,7 @@ def run_worker():
     task_cls = PromptGenerateTask
     worker = MagicWorker(task_cls, sqs_queue_url, region=aws_region)
     logging.info(f'Worker started, listening {sqs_queue_url}')
-    worker.set_task_concurrency(5)
+    worker.set_task_concurrency(20)
     worker.loop_forever()
 
 
